ea4d2780bd779c1cca83f47a26780a94d6cc7a80
LJT <294565790@qq.com>
2025-03-07 18:18:43 +0800

    加入检测展示功能

--- new_thu/UI/main_Pwindow.py
+++ new_thu/UI/main_Pwindow.py
@@ -1,8 +1,12 @@
 from main_viewer import *
 from utils import *
 from typing import cast
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18
+import matplotlib.font_manager as font_manager
 from Cardpage.Two_widget_debug import DIOWidget, AIOWidget_ShowOne
-from PySide2.QtCore import QTimer, QPoint, QRect
+from PySide2.QtCore import QTimer, QPoint, QRect, QObject, Signal
 from PySide2.QtGui import QPixmap, QImage
 from src.molten_pool import CCD_Pretor
 import datetime
@@ -10,6 +14,8 @@ import cv2
 from PIL import Image
 from Cardpage.Signal import g_signals
 import time
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
 import threading
 
 #  D:\\soft\\Anaconda\\envs\\py37\\Scripts\\pyside2-uic -o  E:\Work\THU\code\THU_Project_project\QTui\module\ui_main.py E:\Work\THU\code\THU_Project_project\QTui\main.ui
@@ -200,22 +206,73 @@ class TabWindow(MainWindow):
 
         img_pil.save(path)
 
+    def update_text_browser(self, result_text):
+        self.ui.AIOControlWidget.widgets.textBrowser_ccdres.setText(result_text)  # 在主线程中更新 UI
     def start_recording(self, save_mp4=False, save_img=False):
         if self.mp4_recording:
             print("Already mp4_recording!")
             return
         self.mp4_recording = True
-        # 创建线程
+        # 创建工作线程
         now = datetime.datetime.now()
         create_time = now.strftime("%Y%m%d_%H%M%S")
-        record_thread = threading.Thread(target=self._record_loop, args=(create_time,save_mp4,save_img,))
+        self.worker = Worker()
+        self.worker.update_text_signal.connect(self.update_text_browser)  # 连接信号
+        record_thread = threading.Thread(target=self.worker._record_loop, args=(self, create_time, save_mp4, save_img))
+        # record_thread = threading.Thread(target=self._record_loop, args=(create_time,save_mp4,save_img,))
         record_thread.start()
 
     def _record_loop(self,create_time,save_mp4=False, save_img=False):
+        update_text_signal = Signal(str)
         first_frame = True
         self.fps = self.ui.AIOControlWidget.widgets.Slider_fps.value()
         frame_time = 1 / self.fps  # 每帧期望的时间间隔（秒）
         i = 0
+        #####
+        # 设置中文字体，这里使用系统自带的黑体字体示例，你可以根据实际情况更换为其他支持中文的字体
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+        class_mapping = {
+            '1': 'normal',
+            '2': 'Zero spot voltage',
+            '3': 'No powder',
+            '4': 'No laser'
+        }
+        # 定义图像预处理的转换操作
+        transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),  # 将灰度图转换为3通道图
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        # 初始化模型，这里以ResNet18为例，如果你训练的是其他模型，要做相应替换
+        model = resnet18()
+        num_ftrs = model.fc.in_features
+        # 修改全连接层结构，使其与权重键名对应的结构匹配
+        model.fc = nn.Sequential(
+            nn.Linear(num_ftrs, 4),  # some_output_size需根据实际情况填写，比如分类的类别数量等
+        )
+
+        # 加载训练好的模型权重，替换成你的实际.pth 文件路径
+        checkpoint = torch.load('../resource/models/checkpoint4.pth')
+
+        # 处理权重字典的键名，将fc.weight和fc.bias修改为fc.0.weight和fc.0.bias。
+        # 训练的时候有权重字典的键名的修改，这里要修改
+        new_state_dict = {}
+        for key, value in checkpoint['state_dict'].items():
+            if key == "fc.weight":
+                new_key = "fc.0.weight"
+            elif key == "fc.bias":
+                new_key = "fc.0.bias"
+            else:
+                new_key = key
+            new_state_dict[new_key] = value
+
+        # 使用修改后的权重字典加载权重到模型
+        model.load_state_dict(new_state_dict)
+        model.eval()
 
         while self.mp4_recording:
             start_time = time.time()  # 记录帧处理开始时间
@@ -224,10 +281,46 @@ class TabWindow(MainWindow):
 
             # ===========================
             # img送到网络判读模型
-            # self.ui.AIOControlWidget.textBrowser_ccdres  更新显示结果
-            # ===========================
+            # 应用预处理转换操作
+            # 将 NumPy 数组转换为 PIL 图像
+            img = Image.fromarray(img)
+            img = transform(img).unsqueeze(0)
 
+            # 识别分类
+            with torch.no_grad():
+                outputs = model(img)
+                probabilities = torch.softmax(outputs, dim=1)  # 获取概率分布
+                _, predicted = torch.max(outputs, 1)
+                predicted_class_index = predicted.item()
+                # 因为类别编号从1开始，所以将模型输出的以0为起始的索引值加1
+                predicted_class_index_adjusted = predicted_class_index + 1
+                # 通过调整后的索引从映射关系中获取类别名称
+                predicted_class_name = class_mapping[str(predicted_class_index_adjusted)]
+                predicted_probability = probabilities[0][predicted_class_index].item()  # 获取对应类别的概率
 
+            # # 自定义字体和字号，这里示例设置字体为黑体，字号为12，可根据需求调整
+            # font_path = font_manager.findfont('SimHei')
+            # font_prop = font_manager.FontProperties(fname=font_path, size=12)
+            #
+            # # 文字显示的纵坐标位置偏移量，用于调节打印内容和图片的距离，可根据实际情况微调
+            # text_y_offset = -0.25
+            #
+            # fig, ax = plt.subplots(figsize=(4, 3))  # 设置图像显示的画布大小，可根据需求调整
+            # ax.imshow(img.squeeze(0).permute(1, 2, 0).numpy())
+            # ax.axis('off')
+            #
+            # info_text = f"图像时间: {start_time}\n预测类别: {predicted_class_name}\n该类别的概率: {predicted_probability:.4f}"
+            # ax.text(0.5, text_y_offset, info_text, size=12, ha="center", transform=ax.transAxes,
+            #         fontproperties=font_prop)
+            # 将类别名称和概率转换为字符串
+            result_text = f"预测类别: {predicted_class_name}\n预测概率: {predicted_probability:.2f}"  # 保留两位小数
+            # 更新显示结果
+            # self.ui.AIOControlWidget.widgets.label_ccd_img
+            update_text_signal.emit(result_text)  # 发出信号更新 UI
+            # self.ui.AIOControlWidget.widgets.textBrowser_ccdres.setText(result_text)
+
+            # 将 PIL 图像转换为 NumPy 数组
+            img = np.array(img)
             if isinstance(img, np.ndarray):
                 now = datetime.datetime.now()
                 timestamp = now.strftime("%Y%m%d_%H%M%S")
@@ -398,6 +491,140 @@ class TabWindow(MainWindow):
             self.showMaximized()  # 最大化窗口
             self.is_maximized = True
 
+class Worker(QObject):
+    # 在类中定义信号
+    update_text_signal = Signal(str)
+    def _record_loop(self, self_pwin, create_time, save_mp4=False, save_img=False):
+
+        self_pwin.fps = self_pwin.ui.AIOControlWidget.widgets.Slider_fps.value()
+        frame_time = 1 / self_pwin.fps  # 每帧期望的时间间隔（秒）
+        first_frame = True
+        i = 0
+        # 设置中文字体，这里使用系统自带的黑体字体示例，你可以根据实际情况更换为其他支持中文的字体
+        plt.rcParams['font.sans-serif'] = ['SimHei']
+        plt.rcParams['axes.unicode_minus'] = False
+        class_mapping = {
+            '1': 'normal',
+            '2': 'Zero spot voltage',
+            '3': 'No powder',
+            '4': 'No laser'
+        }
+        # 定义图像预处理的转换操作
+        transform = transforms.Compose([
+            transforms.Grayscale(num_output_channels=3),  # 将灰度图转换为3通道图
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        # 初始化模型，这里以ResNet18为例，如果你训练的是其他模型，要做相应替换
+        model = resnet18()
+        num_ftrs = model.fc.in_features
+        # 修改全连接层结构，使其与权重键名对应的结构匹配
+        model.fc = nn.Sequential(
+            nn.Linear(num_ftrs, 4),  # some_output_size需根据实际情况填写，比如分类的类别数量等
+        )
+
+        # 加载训练好的模型权重，替换成你的实际.pth 文件路径
+        checkpoint = torch.load('../resource/models/checkpoint4.pth')
+
+        # 处理权重字典的键名，将fc.weight和fc.bias修改为fc.0.weight和fc.0.bias。
+        # 训练的时候有权重字典的键名的修改，这里要修改
+        new_state_dict = {}
+        for key, value in checkpoint['state_dict'].items():
+            if key == "fc.weight":
+                new_key = "fc.0.weight"
+            elif key == "fc.bias":
+                new_key = "fc.0.bias"
+            else:
+                new_key = key
+            new_state_dict[new_key] = value
+
+        # 使用修改后的权重字典加载权重到模型
+        model.load_state_dict(new_state_dict)
+        model.eval()
+
+        while self_pwin.mp4_recording:
+            start_time = time.time()  # 记录帧处理开始时间
+            self_pwin.ui.AIOControlWidget.capture(updateshow=True, timedelay=0)
+            img = self_pwin.ui.AIOControlWidget.cam.get_img()
+
+            # img送到网络判读模型
+            # 将 NumPy 数组转换为 PIL 图像
+            img = Image.fromarray(img)
+            img = transform(img).unsqueeze(0)
+
+            # 识别分类
+            with torch.no_grad():
+                outputs = model(img)
+                probabilities = torch.softmax(outputs, dim=1)  # 获取概率分布
+                _, predicted = torch.max(outputs, 1)
+                predicted_class_index = predicted.item()
+                # 因为类别编号从1开始，所以将模型输出的以0为起始的索引值加1
+                predicted_class_index_adjusted = predicted_class_index + 1
+                # 通过调整后的索引从映射关系中获取类别名称
+                predicted_class_name = class_mapping[str(predicted_class_index_adjusted)]
+                predicted_probability = probabilities[0][predicted_class_index].item()  # 获取对应类别的概率
+
+            # 将类别名称和概率转换为字符串
+            result_text = f"预测类别: {predicted_class_name}\n预测概率: {predicted_probability:.2f}\n时间: {start_time}"
+            # 更新显示结果
+            self.update_text_signal.emit(result_text)  # 发出信号更新 UI
+
+            # 将 PIL 图像转换为 NumPy 数组
+            img = np.array(img)
+            if isinstance(img, np.ndarray):
+                now = datetime.datetime.now()
+                timestamp = now.strftime("%Y%m%d_%H%M%S")
+
+                if first_frame:
+                    self_pwin.frame_size = (img.shape[1], img.shape[0])
+                    first_frame = False
+
+                    if save_mp4:
+                        self_pwin.video_save_path = os.path.join(self_pwin.now_select_ccd_save_apppath, f"video_{timestamp}.mp4")
+                        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                        self_pwin.video_writer = cv2.VideoWriter(self_pwin.video_save_path, fourcc, self_pwin.fps, self_pwin.frame_size)
+                        print(f"mp4_recording started: {self_pwin.video_save_path}")
+                    if save_img:
+                        dir = os.path.join(self_pwin.now_select_ccd_save_apppath, f"{create_time}")
+                        if not os.path.exists(dir):
+                            os.makedirs(dir)
+                        print(f"save_img started: {self_pwin.now_select_ccd_save_apppath}")
+
+                resized_img = cv2.resize(img, self_pwin.frame_size)
+                font = cv2.FONT_HERSHEY_SIMPLEX  # 字体
+                font_scale = 0.7  # 字体大小
+                font_color = (0, 255, 255)  # 文字颜色（黄色）
+                thickness = 2  # 字体粗细
+                text_size = cv2.getTextSize(timestamp + f"{i}", font, font_scale, thickness)[0]
+                text_x = resized_img.shape[1] - text_size[0] - 10  # 右上角横坐标
+                text_y = 30  # 右上角纵坐标
+                cv2.putText(resized_img, timestamp + f"{i}", (text_x, text_y), font, font_scale, font_color, thickness)
+                if save_mp4:
+                    self_pwin.video_writer.write(resized_img)
+                if save_img:
+                    path = os.path.join(dir, f"{timestamp}_{i}.jpg")
+                    print(f"save to {path}")
+                    # 假设 img 是 OpenCV 的 numpy 数组
+                    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+                    img_pil.save(path)
+            i += 1
+
+            # 计算实际处理时间和动态调整休眠
+            elapsed_time = time.time() - start_time  # 当前帧的处理时间
+            remaining_time = frame_time - elapsed_time  # 剩余时间
+
+            if remaining_time > 0:
+                time.sleep(remaining_time)  # 补偿时间，保证帧率
+            else:
+                print(f"Warning: Frame took {elapsed_time:.3f}s, which exceeds the target frame time {frame_time:.3f}s")
+
+        if save_mp4:
+            self_pwin.video_writer.release()
+            self_pwin.video_writer = None
+            print(f"mp4_recording stopped. Video saved at: {self_pwin.video_save_path}")
 
 class PWindow(CCD_Window, TabWindow):
     def __init__(self, *args, **kwargs):
